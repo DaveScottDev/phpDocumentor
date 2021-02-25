69505e913c95b06bb3c25af1f4ea52b3b135ecdb
Jaapio <jaap@ijaap.nl>
2021-02-25 20:42:29 +0100

    WIP

--- src/phpDocumentor/Descriptor/ApiSetDescriptor.php
+++ src/phpDocumentor/Descriptor/ApiSetDescriptor.php
@@ -18,13 +18,31 @@ use phpDocumentor\Path;
 
 final class ApiSetDescriptor extends DocumentationSetDescriptor
 {
+    /** @var Collection<FileDescriptor> */
+    private $files;
+
+    /** @var Collection<NamespaceDescriptor> */
+    private $namespaces;
+
     /**
      * @param array{dsn: Dsn, paths: array<Path>} $source
      */
-    public function __construct(string $name, array $source, string $output)
+    public function __construct(string $name, array $source, string $output, Collection $files, Collection $namespaces)
     {
         $this->name = $name;
         $this->source = $source;
         $this->output = $output;
+        $this->files = $files;
+        $this->namespaces = $namespaces;
+    }
+
+    public function getFiles(): Collection
+    {
+        return $this->files;
+    }
+
+    public function getNamespaces(): Collection
+    {
+        return $this->namespaces;
     }
 }
